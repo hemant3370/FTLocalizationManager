2e778631a4e2999e2154a66aa0ce57557c05fd20
Abdulla <abdulla.kunhi@nagarro.com>
2020-04-14 14:46:26 +0400

    FIxed issue while mirroring view

--- FTLocalizationManager/Classes/UIViewExtension.swift
+++ FTLocalizationManager/Classes/UIViewExtension.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 public extension UIView {
-    func forceLeftToRight() {
+    func forceLeftToRight(shouldWithSuviews: Bool = false) {
         guard Language.current.isRTL else { return }
         
         if #available(iOS 13.0, *) {
             semanticContentAttribute = .forceLeftToRight
         } else {
-            transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
+            flip()
+            if !shouldWithSuviews {
+                subviews.forEach { view in
+                    view.flip()
+                }
+            }
         }
+    }
+}
+
+private extension UIView {
+    func flip() {
+        transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
     }
 }
